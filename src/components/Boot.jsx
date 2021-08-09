c3a51b8e7fa5e0b94855622ddb1aaa13e4bf1c2e
P K Mohan <mohanpanathale18@gmail.com>
2021-08-09 15:47:46 +0530

    first commit

---
+++ src/components/Boot.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Boot() {
+  return (
+    <div className="note">
+      <h1>Personnel Details:</h1>
+      <p>Name: P K Mohan</p>
+      <p>DOB: 05-05-2001</p>
+      <p>Mysore</p>
+    </div>
+  );
+}
+
+export default Boot;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>P K Mohan</h1>
+    </header>
+  );
+}
+
+export default Header;
